f83b11f5cac30396db6cf4aa2d9e15f3d634e3be
Kate <90715013+kujoki@users.noreply.github.com>
2023-04-09 11:35:08 +0300

    попытка 1

--- solution.py
+++ solution.py
@@ -1,14 +1,23 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 670223087 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+            x_cnt: int, 
+            y_success: int, 
+            y_cnt: int) -> bool:
+  p1 = x_success / x_cnt
+  p2 = y_success / y_cnt
+  p = (x_success + y_success) / (x_cnt + y_cnt)
+  d = p2 - p1
+  se = (p*(1-p)*(1/x_cnt + 1/y_cnt))**0.5
+  z = d / se
+  alpha = 0.01
+  z_critical = norm.ppf(1 - alpha/2)
+  if abs(z) > z_critical:
+      return True # отклоняем нулевую гипотезу
+  else:
+      return False # не отклоняем нулевую гипотезу
