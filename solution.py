2376fc6441b86f5d873ad7b913b39760048564f0
Kate <90715013+kujoki@users.noreply.github.com>
2023-04-11 14:39:33 +0300

    Попытка № 2 один хвост

--- solution.py
+++ solution.py
@@ -2,22 +2,19 @@ import pandas as pd
 import numpy as np
 from scipy.stats import norm
 
-
 chat_id = 670223087 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, 
-            x_cnt: int, 
-            y_success: int, 
-            y_cnt: int) -> bool:
-  p1 = x_success / x_cnt
-  p2 = y_success / y_cnt
-  p = (x_success + y_success) / (x_cnt + y_cnt)
-  d = p2 - p1
-  se = (p*(1-p)*(1/x_cnt + 1/y_cnt))**0.5
-  z = d / se
-  alpha = 0.01
-  z_critical = norm.ppf(1 - alpha/2)
-  if abs(z) > z_critical:
-      return True # отклоняем нулевую гипотезу
-  else:
-      return False # не отклоняем нулевую гипотезу
+def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
+    p_control = x_success / x_cnt
+    p_test = y_success / y_cnt
+    
+    se_pooled = ((p_control * (1 - p_control) / x_cnt) + (p_test * (1 - p_test) / y_cnt)) ** 0.5
+    
+    z_score = (p_test - p_control) / se_pooled
+    
+    z_critical = norm.ppf(0.01)
+    
+    if z_score <= z_critical:
+        return True 
+    else:
+        return False
