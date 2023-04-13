152adbee048fc0148301b9adb8ae6598d1bb1e0e
Kate <90715013+kujoki@users.noreply.github.com>
2023-04-13 18:25:29 +0300

    Четвертая попытка

--- solution.py
+++ solution.py
@@ -5,5 +5,5 @@ from statsmodels.stats.proportion import proportions_ztest
 chat_id = 670223087 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
-    z_stat, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
+    z_stat, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='smaller')
     return p_value < 0.01
